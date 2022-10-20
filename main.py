5960644b325fdcf81d8214f7ea835f650de3467f
Atatanoff <atatanoff@ya.ru>
2022-10-21 00:26:48 +0400

    фыв

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ TOKEN = "5327216230:AAHrg1tzBgWDWEfy2fDQ-vHWir9Be1qmQ-8"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start', 'help'])  #Явно указываем в декораторе, на какую команду реагируем.
 async def send_welcome(msg: types.Message):
     await msg.reply_to_message(f'Я бот. Приятно познакомиться,{msg.from_user.first_name}')
 
